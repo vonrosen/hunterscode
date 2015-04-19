59f38348bc4f08af951cdcbc695b08256e689cb9
Hunter Stern <hunter@archive.org>
2015-04-19 13:14:16 -0700

    Change

--- src/hunterscode/util/LinkedList.java
+++ src/hunterscode/util/LinkedList.java
@@ -6,7 +6,7 @@ public class LinkedList<E> {
 	private LinkedListNode tail;
 	private int size = 0;
 	
-	protected class LinkedListNode {
+	class LinkedListNode {
 		
 		E obj;
 		LinkedListNode next;
